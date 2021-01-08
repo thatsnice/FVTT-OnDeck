8a56ad2c2e77cba3f4d51493136d9c2f385d211a
Robert de Forest <guitar.robot@gmail.com>
2021-01-08 07:35:53 -0800

    depend on node-jq instead of assuming jq is installed

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,9 @@
 PROJECT_ROOT=$(realpath $(dirname $0)/..)
 SCRIPTS=$PROJECT_ROOT/scripts
 SRC=$PROJECT_ROOT/src
-COFFEE=$PROJECT_ROOT/node_modules/coffeescript/bin/coffee
+NODE=$PROJECT_ROOT/node_modules
+COFFEE=$NODE/coffeescript/bin/coffee
+JQ=$NODE/node-jq/bin/jq
 
 if ! [ -x $COFFEE ]; then
   npm install
@@ -10,4 +12,4 @@ fi
 mkdir   -p $SCRIPTS
 $COFFEE -o $SCRIPTS $SRC
 $COFFEE $PROJECT_ROOT/tools/version-bump.coffee
-echo "Version bumped to $(jq .version module.json)"
+echo "Version bumped to $($JQ .version module.json)"
